adbbefa94de26600f55ad7e106e230ed268084fe
Nikita <lykyanov2005@mail.ru>
2023-12-20 21:16:24 +0700

    Функция 1

--- 4lab.py
+++ 4lab.py
@@ -2,7 +2,12 @@ import math
 
 
 def cg_distance(x1, y1, x2, y2, x3, y3, x4, y4) -> float:  #центры
-    pass
+    Ax = (x1 + x2) / 2
+    Ay = (y1 + y2) / 2
+    Bx = (x3 + x4) / 2
+    By = (y3 + y4) / 2
+    AB = math.sqrt((Ax - Bx) ** 2 + (Ay - By) ** 2)
+    return round(AB, 2)
 
 
 def corner_distance(x1, y1, x2, y2, x3, y3, x4, y4) -> float:  #углы
